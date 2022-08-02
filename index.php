70815f6287864ce3d21193fec2b80731f34334e4
Jjosesamayoa <Jose18vasquez.t@gmail.com>
2022-08-02 13:15:44 -0600

    Login y mensajería subidos primera parte

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="dindex.css"> <link rel="stylesheet" href="dindex2.css">
+    <link rel="stylesheet" href="dindex.css">
+    <link rel="stylesheet" href="dindex2.css">
     <title>Toyota</title>
 </head>
 
@@ -18,19 +19,19 @@
         </tr>
         <tr>
             <td colspan="4">
-                    <form action="productos/productos.php">
-                        <input type="submit" value="Productos">
-                    </form>
+                <form action="productos.php">
+                    <input type="submit" value="Productos">
+                </form>
             </td>
             <td colspan="4">
-                    <form action="tiendas/tiendas.php">
-                        <input type="submit" value="Tiendas">
-                    </form>
+                <form action="tiendas.php">
+                    <input type="submit" value="Tiendas">
+                </form>
             </td>
             <td colspan="4">
-                    <form action="contactos/contacto.php">
-                        <input type="submit" value="Contáctenos">
-                    </form>
+                <form action="lg.php">
+                    <input type="submit" value="Contáctenos">
+                </form>
             </td>
         </tr>
         <tr>
@@ -54,11 +55,11 @@
                 <h6 style="color: left;">Dirección: XXXXX-XXXXX-XXXX</h6>
             </td>
             <td colspan="2" align="right">
-            <h6>Carné: 2021-40131</h6>
+                <h6>Carné: 2021-40131</h6>
             </td>
             <td colspan="4">.</td>
             <td colspan="2" align="right">
-            <h6>Nombre: Juan José Samayoa Vásquez</h6>
+                <h6>Nombre: Juan José Samayoa Vásquez</h6>
             </td>
         </tr>
     </table>
