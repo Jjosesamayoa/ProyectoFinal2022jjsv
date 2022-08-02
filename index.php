c68b5acc86761edb5837d34008f4374e18b7a778
Jjosesamayoa <Jose18vasquez.t@gmail.com>
2022-08-02 07:47:49 -0600

    Creo la página 1 del proyecto

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="dindex.css"> <link rel="stylesheet" href="dindex2.css">
+    <title>Toyota</title>
+</head>
+
+<body style="background: url(image/fondo.jpg); background-size: cover; background-repeat: no-repeat;">
+    <table width="100%">
+        <tr>
+            <td colspan="12" align="center">
+                <a href="image/logo.png"><img src="image/logo.png" width="600" height="400px"></a>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                    <form action="productos.php">
+                        <input type="submit" value="Productos">
+                    </form>
+            </td>
+            <td colspan="4">
+                    <form action="tiendas.php">
+                        <input type="submit" value="Tiendas">
+                    </form>
+            </td>
+            <td colspan="4">
+                    <form action="contacto.php">
+                        <input type="submit" value="Contáctenos">
+                    </form>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="12" align="left"><br>
+                <h3 style="color: purple;">Ofertas del mes</h3>
+            </td>
+        </tr>
+    </table><br>
+    <div class="contenedor">
+        <section class="a" align="center"><a href="image/lg.png"><img src="image/lg.png" width="330px" height="300px"></a></section>
+        <section class="b" align="center"><a href="image/img1.png"><img src="image/img1.png" width="250px" height="250px"></a></section>
+        <section class="c" align="center"><a href="image/img2.png"><img src="image/img2.png" width="260px" height="200px"></a></section>
+        <section class="d" align="center"><a href="image/img3.png"><img src="image/img3.png" width="500px" height="230px"></a></section>
+    </div>
+    <table>
+        <tr>
+            <td colspan="3" style="background-color: purple;">
+                <h6 style="color: withe;">PBX: XXXXX-XXXXX</h6>
+            </td>
+            <td colspan="3" style="background-color: purple;">
+                <h6 style="color: withe;">Dirección: XXXXX-XXXXX-XXXX</h6>
+            </td>
+            <td colspan="3" style="background-color: purple;" align="right">
+            <h6>Carné: 2021-40131</h6>
+            </td>
+            <td colspan="3" style="background-color: purple;" align="right">
+            <h6>Nombre: Juan José Samayoa Vásquez</h6>
+            </td>
+        </tr>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
