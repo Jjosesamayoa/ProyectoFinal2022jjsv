1e618b8fbb0c844ea4876868fa48031c883965f0
Jjosesamayoa <Jose18vasquez.t@gmail.com>
2022-08-05 12:12:44 -0600

    Se trabajó parte de mensajería y productos

--- mensajeria/mensajeria.php
+++ mensajeria/mensajeria.php
@@ -14,6 +14,7 @@ if (!isset($us)) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="mensajeria.css">
+<link rel="stylesheet" href="boton.css">
 
 <body>
 
@@ -28,33 +29,33 @@ if (!isset($us)) {
                 </form>
             </td>
             <td colspan="4">
-                <form action="productos.php">
+                <form action="../productos/productos.php">
                     <input type="submit" value="Productos">
                 </form>
             </td>
             <td colspan="4">
-                <form action="tiendas.php">
+                <form action="../tiendas/tiendas.php">
                     <input type="submit" value="Tiendas">
                 </form>
             </td>
         </tr>
     </table>
-    <form class="w3-container" action="#">
+    <form class="w3-container" action="enviar.php" method="post">
         <p>
             <label>Nombres</label>
-            <input class="w3-input" type="text">
+            <input class="w3-input" type="text" placeholder="<?php echo $us; ?>" name="a">
         </p>
         <p>
             <label>Teléfono/Celular</label>
-            <input class="w3-input" type="text">
+            <input class="w3-input" type="text" name="b">
         </p>
         <p>
             <label>Correo Electrónico</label>
-            <input class="w3-input" type="text">
+            <input class="w3-input" type="text" name="c">
         </p>
         <label>Mensaje</label>
-        <input class="w3-input" type="text"></p>
-        <input type="submit" value="Enviar Mensaje">
+        <input class="w3-input" type="text" name="d"></p>
+        <input type="submit" value="Enviar Mensaje" name="e">
     </form>
     <table width="100%" align="center">
         <tr>
@@ -62,6 +63,17 @@ if (!isset($us)) {
                 <a href="cerrar.php" style="text-decoration: none;">
                     <h5 style="background-color: red;">Cerrar Sesión</h5>
                 </a>
+            </td>
+        </tr>
+    </table>
+    <table width="100%">
+        <tr>
+            <td>
+                <form action="administradores.php">
+                    <div class="cont">
+                        <button type="submit" class="boton">¿Eres administrador?, ingresa para revisar mensajes</button>
+                    </div>
+                </form>
             </td>
         </tr>
     </table>
--- productos/productos.php
+++ productos/productos.php
@@ -14,28 +14,137 @@
     <table width="100%">
         <tr>
             <td colspan="4">
-                <form action="productos.php">
+                <form action="#">
                     <input type="submit" value="Productos">
                 </form>
             </td>
             <td colspan="4">
-                <form action="tiendas.php">
+                <form action="../tiendas/tiendas.php">
                     <input type="submit" value="Tiendas">
                 </form>
             </td>
             <td colspan="4">
-                <form action="lg.php">
+                <form action="../mensajeria/lg.php">
                     <input type="submit" value="Contáctenos">
                 </form>
             </td>
         </tr>
         <tr>
             <td colspan="12" align="left"><br>
-                <h3 style="color: purple;">Productos/Servicios</h3>
+                <h3 style="color: white;">Productos/Servicios</h3>
             </td>
         </tr>
     </table>
     <table width="100%">
+        <tr>
+            <td>
+                <section class="a">
+                    <div class="contenedor">
+                        <figure>
+                            <img src="image/S2A-23.jpg">
+                            <div class="capa">
+                                <h3>RAV4</h3>
+                                <p>Modelo SA2-23 Gasolina,16 Valvulas, DOHC, Dual VVTi, 203/4400-4900 Nm/rpm. M20A-FKS <br>Q250,000</p>
+                            </div>
+                        </figure>
+                    </div>
+                </section>
+            </td>
+            <td>
+                <section class="b">
+                    <div class="contenedor">
+                        <figure>
+                            <img src="image/S2M-23.jpg">
+                            <div class="capa">
+                                <h3>RAV4</h3>
+                                <p>Modelo S2M-23 Cuero local Gratis<br>Q250,000 </p>
+                            </div>
+                        </figure>
+                    </div>
+                </section>
+            </td>
+            <td>
+                <section class="c">
+                    <div class="contenedor">
+                        <figure>
+                            <img src="image/S2A-23.jpg">
+                            <div class="capa">
+                                <h3>RAV4</h3>
+                                <p>Modelo S2T-23 Gasolina,16 Valvulas,DOHC,Dual VVTi, 203/4400-4900 Nm/rpm. M20A-FKS<br>Q265,999 </p>
+                            </div>
+                        </figure>
+                    </div>
+                </section>
+            </td>
+            <td>
+                <section class="d">
+                    <div class="contenedor">
+                        <figure>
+                            <img src="image/MU-23.jpg">
+                            <div class="capa">
+                                <h3>Toyota Yaris</h3>
+                                <p>Modelo MU-23 Gasolina,VVT-i Dual,16Válvulas DOHC 123/4200 Nm/rpm. 1NR-FE<br>Q125,900 </p>
+                            </div>
+                        </figure>
+                    </div>
+                </section>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <section class="e">
+                    <div class="contenedor">
+                        <figure>
+                            <img src="image/VX-23.jpg">
+                            <div class="capa">
+                                <h3>Toyota Corolla Cross Gasolina</h3>
+                                <p>Modelo VX-23 Gasolina,16 Valvulas,DOHC,Dual VVTi, 203/4400-4900 Nm/rpm. M20A-FKS <br>Q215,990</p>
+                            </div>
+                        </figure>
+                    </div>
+                </section>
+            </td>
+            <td>
+                <section class="f">
+                    <div class="contenedor">
+                        <figure>
+                            <img src="image/CHR2-23.jpg">
+                            <div class="capa">
+                                <h3>CH R Hibrida</h3>
+                                <p>Modelo CHR2-23 Gasolina, 16 válvulas, VVTi, DOHC, 142/3600 Nm/rpm. 2ZR-FXE. Motor eléctrico de 72 H.P. 163/Nm Híbrido<br>Q257,990 </p>
+                            </div>
+                        </figure>
+                    </div>
+                </section>
+            </td>
+            <td>
+                <section class="g">
+                    <div class="contenedor">
+                        <figure>
+                            <img src="image/CHR3-23.jpg">
+                            <div class="capa">
+                                <h3>CH R Hibrida</h3>
+                                <p>Modelo chr3-23 Gasolina, 16 válvulas, VVTi, DOHC, 142/3600 Nm/rpm. 2ZR-FXE. Motor eléctrico de 72 H.P. 163/Nm Híbrido<br>Q262,990 </p>
+                            </div>
+                        </figure>
+                    </div>
+                </section>
+            </td>
+            <td>
+                <section class="h">
+                    <div class="contenedor">
+                        <figure>
+                            <img src="image/PA-23.jpg">
+                            <div class="capa">
+                                <h3>Toyota Yaris Sedan</h3>
+                                <p>Modelo PA-23 Gasolina,VVT-i Dual,16Válvulas DOHC, 140/4200 Nm/rpm. 2NR-FE<br>Q175,900 </p>
+                            </div>
+                        </figure>
+                    </div>
+                </section>
+            </td>
+        </tr>
+        
         <tr>
             <td>
                 <section class="a">
