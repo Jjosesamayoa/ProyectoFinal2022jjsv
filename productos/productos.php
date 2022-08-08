f1cd92d3119a0114077efeb1868d56b14592835a
Jjosesamayoa <Jose18vasquez.t@gmail.com>
2022-08-08 10:48:42 -0600

    Página de revisión de mensajes lista

--- productos/productos.php
+++ productos/productos.php
@@ -14,8 +14,8 @@
     <table width="100%">
         <tr>
             <td colspan="4">
-                <form action="#">
-                    <input type="submit" value="Productos">
+                <form action="../index.php">
+                    <input type="submit" value="Pa. Inicio">
                 </form>
             </td>
             <td colspan="4">
@@ -147,106 +147,53 @@
         
         <tr>
             <td>
-                <section class="a">
+                <section class="i">
                     <div class="contenedor">
                         <figure>
-                            <img src="image/S2A-23.jpg">
+                            <img src="image/BU-23.jpg">
                             <div class="capa">
-                                <h3>RAV4</h3>
-                                <p>Modelo SA2-23 Gasolina,16 Valvulas, DOHC, Dual VVTi, 203/4400-4900 Nm/rpm. M20A-FKS <br>Q250,000</p>
+                                <h3>Toyota Innova</h3>
+                                <p>Modelo BU-23 Gasolina,VVTi,16 Válvulas, DOHC 245/4000 Nm/rpm. 2TR-FE Caja automática de 6 velocidades y retroceso <br>Q205,990</p>
                             </div>
                         </figure>
                     </div>
                 </section>
             </td>
             <td>
-                <section class="b">
+                <section class="j">
                     <div class="contenedor">
                         <figure>
-                            <img src="image/S2M-23.jpg">
+                            <img src="image/GK-23.jpg">
                             <div class="capa">
-                                <h3>RAV4</h3>
-                                <p>Modelo S2M-23 Cuero local Gratis<br>Q250,000 </p>
+                                <h3>Toyota Raize</h3>
+                                <p>Modelo GK-23 Gasolina, Turbo, Intercooler, 12 Válvulas, DOHC, DVVT, 140Nm/2400-4000rpm. 1KR-VET Caja continuamente variable y secuencial de 7 velocidades y retroceso<br>Q186,990 </p>
                             </div>
                         </figure>
                     </div>
                 </section>
             </td>
             <td>
-                <section class="c">
+                <section class="k">
                     <div class="contenedor">
                         <figure>
-                            <img src="image/S2A-23.jpg">
+                            <img src="image/LC-23.jpg">
                             <div class="capa">
-                                <h3>RAV4</h3>
-                                <p>Modelo S2T-23 Gasolina,16 Valvulas,DOHC,Dual VVTi, 203/4400-4900 Nm/rpm. M20A-FKS<br>Q265,999 </p>
+                                <h3>Toyota Raize</h3>
+                                <p>Modelo LC-23 Gasolina, 12 Válvulas, DOHC, Dual-VVT, 113Nm/4500rpm. WA-VE<br>Q156,990 </p>
                             </div>
                         </figure>
                     </div>
                 </section>
             </td>
             <td>
-                <section class="d">
+                <section class="l">
                     <div class="contenedor">
                         <figure>
-                            <img src="image/MU-23.jpg">
+                            <img src="image/J10F8-23.png">
                             <div class="capa">
-                                <h3>Toyota Yaris</h3>
-                                <p>Modelo MU-23 Gasolina,VVT-i Dual,16Válvulas DOHC 123/4200 Nm/rpm. 1NR-FE<br>Q125,900 </p>
-                            </div>
-                        </figure>
-                    </div>
-                </section>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <section class="e">
-                    <div class="contenedor">
-                        <figure>
-                            <img src="image/VX-23.jpg">
-                            <div class="capa">
-                                <h3>Toyota Corolla Cross Gasolina</h3>
-                                <p>Modelo VX-23 Gasolina,16 Valvulas,DOHC,Dual VVTi, 203/4400-4900 Nm/rpm. M20A-FKS <br>Q215,990</p>
-                            </div>
-                        </figure>
-                    </div>
-                </section>
-            </td>
-            <td>
-                <section class="f">
-                    <div class="contenedor">
-                        <figure>
-                            <img src="image/CHR2-23.jpg">
-                            <div class="capa">
-                                <h3>CH R Hibrida</h3>
-                                <p>Modelo CHR2-23 Gasolina, 16 válvulas, VVTi, DOHC, 142/3600 Nm/rpm. 2ZR-FXE. Motor eléctrico de 72 H.P. 163/Nm Híbrido<br>Q257,990 </p>
-                            </div>
-                        </figure>
-                    </div>
-                </section>
-            </td>
-            <td>
-                <section class="g">
-                    <div class="contenedor">
-                        <figure>
-                            <img src="image/CHR3-23.jpg">
-                            <div class="capa">
-                                <h3>CH R Hibrida</h3>
-                                <p>Modelo chr3-23 Gasolina, 16 válvulas, VVTi, DOHC, 142/3600 Nm/rpm. 2ZR-FXE. Motor eléctrico de 72 H.P. 163/Nm Híbrido<br>Q262,990 </p>
-                            </div>
-                        </figure>
-                    </div>
-                </section>
-            </td>
-            <td>
-                <section class="h">
-                    <div class="contenedor">
-                        <figure>
-                            <img src="image/PA-23.jpg">
-                            <div class="capa">
-                                <h3>Toyota Yaris Sedan</h3>
-                                <p>Modelo PA-23 Gasolina,VVT-i Dual,16Válvulas DOHC, 140/4200 Nm/rpm. 2NR-FE<br>Q175,900 </p>
+                                <h3>Land Cruiser Hardtop</h3>
+                                <p>Modelo J10F8-23 Diesel,Turbo Intercooler DOHC,32 Válvulas,COMMON RAIL, 430/1200- 3200 NM/rpm
+<br>Q529,990 </p>
                             </div>
                         </figure>
                     </div>
