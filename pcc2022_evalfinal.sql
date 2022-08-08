bfb6b0995f8f4047095fc01af5b2fcb70ddcd854
Jjosesamayoa <Jose18vasquez.t@gmail.com>
2022-08-08 10:58:00 -0600

    base nueva

--- pcc2022_evalfinal.sql
+++ pcc2022_evalfinal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-08-2022 a las 19:20:55
+-- Tiempo de generación: 08-08-2022 a las 18:57:20
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 7.4.27
 
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `admins`
+--
+
+CREATE TABLE `admins` (
+  `uss` varchar(100) NOT NULL,
+  `passw` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `admins`
+--
+
+INSERT INTO `admins` (`uss`, `passw`) VALUES
+('LCP', '123');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `lgjjsv`
 --
 
@@ -40,15 +58,52 @@ INSERT INTO `lgjjsv` (`Usuario`, `Contrasena`) VALUES
 ('LCP', '123'),
 ('Samayoa', '1618');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `id` int(200) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `tel` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `mensaje` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`uss`);
 
 --
 -- Indices de la tabla `lgjjsv`
 --
 ALTER TABLE `lgjjsv`
   ADD PRIMARY KEY (`Usuario`);
+
+--
+-- Indices de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
