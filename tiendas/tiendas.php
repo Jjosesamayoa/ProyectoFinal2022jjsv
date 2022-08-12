8f3670533d39c6664d7c10f9a6bdc2425656c99c
Jjosesamayoa <Jose18vasquez.t@gmail.com>
2022-08-12 10:57:51 -0600

    Actualización en tiendas

---
+++ tiendas/tiendas.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="d.css">
+    <title>Tiendas</title>
+</head>
+
+<body style="background-color: lightgreen;">
+    <table width="100%">
+        <tr>
+            <td colspan="4">
+                <section class="a">
+                    <form action="../productos/productos.php">
+                        <input type="submit" value="Productos">
+                    </form>
+                </section>
+            </td>
+            <td colspan="4">
+                <section class="b">
+                    <form action="../index.php">
+                        <input type="submit" value="Pg.Inicio">
+                    </form>
+                </section>
+            </td>
+            <td colspan="4">
+                <section class="c">
+                    <form action="../mensajeria/mensajeria.php">
+                        <input type="submit" value="Contactenos">
+                    </form>
+                </section>
+            </td>
+        </tr>
+    </table>
+    <div class="container" border="1">
+        <div class="row align-items-start">
+            <div class="col-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61791.48480204028!2d-90.57843157063468!3d14.543834098260922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a3d9b4844d35%3A0xd73cc2651e4e415e!2sToyota%20Bulevar%20Liberaci%C3%B3n!5e0!3m2!1ses-419!2sgt!4v1660053140128!5m2!1ses-419!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 22040404</p><br>
+                <p>Dirección: Boulevar Liberación 4-59 Z. 9 PBX: 1705, Cdad. de Guatemala 01009</p><br>
+                <p>Correo: contactotoyota@toyota.gt</p><br>
+                <p>Horario: Lunes a viernes, 9:00 - 19:00</p><br>
+            </div>
+        </div><br>
+        <div class="row align-items-center">
+            <div class="col-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61770.570330327464!2d-90.58217544179689!3d14.618397600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a2315e0c1959%3A0xfc170d79b9332a7e!2sToyota%20Cofi%C3%B1o%20Stahl!5e0!3m2!1ses-419!2sgt!4v1660053921763!5m2!1ses-419!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 22040404</p><br>
+                <p>Dirección: 10 Avenida, 31-71, Zona 5. Guatemala, Cdad. de Guatemala 01005</p><br>
+                <p>Correo: contactotoyota@toyota.gt</p><br>
+                <p>Horario: Lunes a viernes, 6:00 - 18:00</p><br>
+            </div>
+        </div><br>
+        <div class="row align-items-start">
+            <div class="col-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61766.02941376202!2d-90.65395634179686!3d14.634537600000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a06acb3691e5%3A0x84c026607841cf7!2sTOYOTA%20GUATEMALA!5e0!3m2!1ses-419!2sgt!4v1660054123358!5m2!1ses-419!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 22040404</p><br>
+                <p>Dirección Pan-American Highway 5, Cdad. de Guatemala</p><br>
+                <p>Correo: contactotoyota@toyota.gt</p><br>
+                <p>Horario: Lunes a viernes, 6:00 - 18:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61766.02941376202!2d-90.65395634179686!3d14.634537600000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a06acb3691e5%3A0x84c026607841cf7!2sTOYOTA%20GUATEMALA!5e0!3m2!1ses-419!2sgt!4v1660054123358!5m2!1ses-419!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 22040404</p><br>
+                <p>Dirección Pan-American Highway 5, Cdad. de Guatemala</p><br>
+                <p>Correo: contactotoyota@toyota.gt</p><br>
+                <p>Horario: Lunes a viernes, 6:00 - 18:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-start">
+            <div class="col-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61766.02941376202!2d-90.65395634179686!3d14.634537600000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a06acb3691e5%3A0x84c026607841cf7!2sTOYOTA%20GUATEMALA!5e0!3m2!1ses-419!2sgt!4v1660054123358!5m2!1ses-419!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 22040404</p><br>
+                <p>Dirección Pan-American Highway 5, Cdad. de Guatemala</p><br>
+                <p>Correo: contactotoyota@toyota.gt</p><br>
+                <p>Horario: Lunes a viernes, 6:00 - 18:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61766.02941376202!2d-90.65395634179686!3d14.634537600000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a06acb3691e5%3A0x84c026607841cf7!2sTOYOTA%20GUATEMALA!5e0!3m2!1ses-419!2sgt!4v1660054123358!5m2!1ses-419!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 22040404</p><br>
+                <p>Dirección Pan-American Highway 5, Cdad. de Guatemala</p><br>
+                <p>Correo: contactotoyota@toyota.gt</p><br>
+                <p>Horario: Lunes a viernes, 6:00 - 18:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-start">
+            <div class="col-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61766.02941376202!2d-90.65395634179686!3d14.634537600000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a06acb3691e5%3A0x84c026607841cf7!2sTOYOTA%20GUATEMALA!5e0!3m2!1ses-419!2sgt!4v1660054123358!5m2!1ses-419!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 22040404</p><br>
+                <p>Dirección Pan-American Highway 5, Cdad. de Guatemala</p><br>
+                <p>Correo: contactotoyota@toyota.gt</p><br>
+                <p>Horario: Lunes a viernes, 6:00 - 18:00</p><br>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61766.02941376202!2d-90.65395634179686!3d14.634537600000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a06acb3691e5%3A0x84c026607841cf7!2sTOYOTA%20GUATEMALA!5e0!3m2!1ses-419!2sgt!4v1660054123358!5m2!1ses-419!2sgt" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-4">
+                <p>Tel: 22040404</p><br>
+                <p>Dirección Pan-American Highway 5, Cdad. de Guatemala</p><br>
+                <p>Correo: contactotoyota@toyota.gt</p><br>
+                <p>Horario: Lunes a viernes, 6:00 - 18:00</p><br>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
