54ac85afacb16643950106eefa9869c8ccc60cb1
Jjosesamayoa <Jose18vasquez.t@gmail.com>
2022-09-12 20:32:01 -0600

    Páginas por producto

---
+++ servicios/l.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body style="background-color: rgba(190, 240, 255, 1.457);">
+    <table width="100%" align="center">
+        <tr>
+            <td colspan="12" align="center"><img src="a/rav4-2020-color-rojo-nueva.webp" width="60%"></td>
+        </tr>
+    </table><br><br><br>
+    <table width="100%" style="background-color: rgba(200, 0, 255, 0.457);">
+        <tr>
+            <td colspan="4">
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:35px;" align="center">Diseño audaz y elegante</p>
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:20px;" align="center">>La quinta generación de RAV4 presenta un diseño más robusto que brinda el refinamiento característico y dinámico de la popular SUV de Toyota</p>
+            </td>
+            <td colspan="4">
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:35px;" align="center">Compuerta trasera</p>
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:20px;" align="center">>La nueva RAV4 al igual que su antecesora, cuenta con un diseño funcional en la compuerta trasera y logra ampliar el compartimiento de equipaje 65mm más que su versión anterior</p>
+            </td>
+            <td colspan="4">
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:35px;" align="center">Desempeño</p>
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:20px;" align="center">>Rendimiento mejorado para superar cualquier obstáculo. Las capacidades y potencial para vivir las más grandes aventuras dentro o fuera de la carretera, ahora con mayor eficiencia.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:35px;" align="center">Control de par dinámico 4WD</p>
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:20px;" align="center">>El sistema permite al conductor rastrear la línea de conducción prevista y admite la conducción versátil en diversas superficies. El par de la rueda trasera se ajusta con precisión en respuesta a un exceso o una deficiencia de la velocidad de guiñada en las curvas. Esto ayuda a realizar un rendimiento ideal en las curvas. (Estándar en modelos 4WD equipados con VSC)</p>
+            </td>
+            <td colspan="4">
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:35px;" align="center">Control de asistencia de arranque en colina</p>
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:20px;" align="center">>Seguridad y control al conducir cuando atraviesas un terreno inclinado con el Sistema HAC (Hill Assist Control); a través de una lectura de los sensores que detectan la inclinación del terreno, evita el desplazamiento hacia atrás del vehículo.</p>
+            </td>
+            <td colspan="4">
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:35px;" align="center">motor</p>
+                <p style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; font-size:20px;" align="center">>La nueva RAV4 cuenta con un motor de 2.0 L y 170 caballos de fuerza, con mayor economía de combustible con respecto a su versión anterior.</p>
+            </td>
+        </tr>
+    </table><br><br>
+    <table align="center">
+        <tr>
+            <td colspan="6"><img src="a/rav4-Galeria-Ext-2.webp" width="600px" height="300px"></td>
+            <td colspan="3"><img src="a/rav4-Galeria-Ext-3.webp" width="350px" height="300px"></td>
+            <td colspan="3"><img src="a/rav4-Galeria-Int-3.webp" width="350px" height="300px"></td>
+        </tr>
+    </table>
+    <table align="center">>
+        <tr>
+            <td colspan="3"><img src="a/rav4-Galeria-Int-2.webp" width="350px" height="300px"></td>
+            <td colspan="3"><img src="a/rav4-Galeria-Int-4.webp" width="350px" height="300px"></td>
+            <td colspan="6"><img src="a/rav4-Galeria-Int-5.webp" width="600px" height="300px"></td>
+        </tr>
+    </table>
+    <table align="center">>
+        <td colspan="6"><img src="a/Accesorios-Tapiceria-RAV4-2020-860x390.webp" width="700px" height="300px"></td>
+        <td colspan="6"><img src="a/rav4-Galeria-Int-3 (1).webp" width="607px" height="300px"></td>
+    </table>
+</body>
+
+</html>
